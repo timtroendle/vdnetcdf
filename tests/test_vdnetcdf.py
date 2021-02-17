2a66de5a93e8f38c6b858843b0f42df823ad7cc4
Tim Tröndle <tim.troendle@usys.ethz.ch>
2021-02-17 13:33:03 +0100

    Fix test

--- tests/test_vdnetcdf.py
+++ tests/test_vdnetcdf.py
@@ -2,5 +2,5 @@
 
 
 def test_can_replay_read_of_netcdf(bash):
-    output = bash.send("vd -b -p tests/sample_data/cmdlog-01.vd").split("\n")
-    assert "replay complete" in output[-1]
+    bash.send("vd -b -p tests/sample_data/cmdlog-01.vd").split("\n")
+    assert bash.last_return_code == 0
