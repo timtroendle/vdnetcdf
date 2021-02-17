699bea591be87884da955d7acc1b959ffc549d66
Tim Tröndle <tim.troendle@usys.ethz.ch>
2021-02-17 13:21:31 +0100

    Add VisiData 2.0 compatibility

--- vdnetcdf/loader.py
+++ vdnetcdf/loader.py
@@ -1,27 +1,25 @@
-from visidata import Sheet, Column, ColumnItem, anytype, date, asyncthread, Progress, ENTER
+from visidata import IndexSheet, TableSheet, Column, ItemColumn, Progress, anytype, date
 
 
 def open_nc(path):
     return DatasetSheet(path.name, source=path)
 
 
-class DatasetSheet(Sheet):
+class DatasetSheet(IndexSheet):
     rowtype = 'variables'  # rowdef: xr.DataArray
 
     columns = [
-        Column('variable', getter=lambda col, row: row[0]),
-        Column('dtype', getter=lambda col, row: row[1].dtype),
-        Column('dimensions', getter=lambda col, row: row[1].dims),
-        Column('size', getter=lambda col, row: row[1].size)
+        Column('variable', getter=lambda col, row: row.source.name),
+        Column('dtype', getter=lambda col, row: row.source.dtype),
+        Column('size', getter=lambda col, row: row.source.size),
+        Column('dimensions', getter=lambda col, row: row.source.dims),
     ]
 
-    def reload(self):
+    def iterload(self):
         import xarray as xr
         ds = xr.open_dataset(str(self.source))
-        self.rows = []
         for r in ds.data_vars.items():
-            vs = DataArraySheet(r[0], source=r[1])
-            self.rows.append((r[0], r[1], vs))
+            yield DataArraySheet(source=r[1])
 
     def attributeSheet(self):
         import xarray as xr
@@ -29,26 +27,25 @@ class DatasetSheet(Sheet):
         return AttributeSheet(f"{self.name}-attrs", source=ds)
 
 
-class DataArraySheet(Sheet):
-    rowtype = 'data'  # rowdef: map
+class DataArraySheet(TableSheet):
+    rowtype = 'datapoints'  # rowdef: map
 
-    @asyncthread
-    def reload(self):
+    def iterload(self):
         da = self.source
 
         self.columns = []
 
         for dim in list(da.dims) + [coords for coords in da.coords if coords not in da.dims]:
-            self.addColumn(ColumnItem(dim, type=type_mapping(da[dim].dtype)))
-        self.addColumn(ColumnItem(da.name, type=type_mapping(da.dtype)))
-
-        self.rows = da.to_dataframe().reset_index().to_dict(orient="records")
+            self.addColumn(ItemColumn(dim, type=type_mapping(da[dim].dtype)))
+        self.addColumn(ItemColumn(da.name, type=type_mapping(da.dtype)))
+        for row in Progress(da.to_dataframe().reset_index().iterrows(), total=da.size):
+            yield row[1].to_dict()
 
     def attributeSheet(self):
         return AttributeSheet(f"{self.name}-attrs", source=self.source)
 
 
-class AttributeSheet(Sheet):
+class AttributeSheet(TableSheet):
     rowtype = 'attributes'  # rowdef: attributes
 
     columns = [
@@ -56,14 +53,11 @@ class AttributeSheet(Sheet):
         Column('value', getter=lambda col, row: row[1]),
     ]
 
-    @asyncthread
-    def reload(self):
-        self.rows = []
+    def iterload(self):
         for k, v in Progress(self.source.attrs.items()):
-            self.rows.append((k, v))
+            yield (k, v)
 
 
-DatasetSheet.addCommand(ENTER, 'dive-row', 'vd.push(cursorRow[2])')
 DatasetSheet.addCommand('^A', 'show-attributes', 'vd.push(attributeSheet())')
 DataArraySheet.addCommand('^A', 'show-attributes', 'vd.push(attributeSheet())')
 
